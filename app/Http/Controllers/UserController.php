53ea9fa41d45b5ac1bdffbaf4aa6a401ef0193f9
Jeferson Chagas Silva <jeferson_chagas25@hotmail.com>
2024-04-09 17:05:53 -0300

    feat: Criando componente para formulário.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,4 +10,9 @@ class UserController extends Controller
     {
         return view('user.create');
     }
+
+    public function store()
+    {
+        dd('store');
+    }
 }
--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -9,4 +9,21 @@
     </x-slot>
 
     <h2>User Create</h2>
+
+    <x-form action="{{ route('user.store') }}" method="post">
+        <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+        </div>
+
+        <button type="submit">Create</button>
+    </x-form>
 </x-layout>
