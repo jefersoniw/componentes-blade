cfc2a8c68e41c12db6d3edf57a486933f142fc3d
Jeferson Chagas Silva <jeferson_chagas25@hotmail.com>
2024-04-10 12:41:56 -0300

    feat: criando componentes para os inputs do form.

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -11,18 +11,10 @@
     <h2>User Create</h2>
 
     <x-form action="{{ route('user.store') }}" method="post">
-        <div class="mb-3">
-            <label for="name" class="form-label">Name</label>
-            <input type="text" class="form-control" id="name" name="name" placeholder="Name">
-        </div>
-        <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" name="email" placeholder="Email">
-        </div>
-        <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-        </div>
+
+        <x-form.input-text name="name" label="Name" placeholder="Name..." />
+        <x-form.input-text type="email" name="email" label="Email" placeholder="Email..." />
+        <x-form.input-text type="password" name="password" label="Password" placeholder="Password..." />
 
         <button type="submit">Create</button>
     </x-form>
